f04a8de85835d390beee361a9e1e45f2f5d28812
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-05-13 15:27:27 +0200

    [scons] do not add InfusionArchiveObject method

--- scons/infuser.py
+++ scons/infuser.py
@@ -21,13 +21,6 @@ import os
 from SCons.Script import SConscript, File, Depends
 import SCons.Util
 
-def infusion_archive_object_method(target, source, env):
-	""" takes a list of *.di files, turns them into an ar archive;
-	    the raw bytes of the archive will then be included in a
-	    c array which will be turned into an object file """
-	print("TODO: implement infusion_archive_object_method called")
-	return []
-
 def infusion_action_generator(target, source, env, for_signature):
 	flags = {'.dih': '-h', '.di': '-o', '.h': '-d', '.c': '-c'}
 	name = os.path.basename(os.path.dirname(str(target[0])))
@@ -95,16 +88,12 @@ def generate(env):
 
 	# 3.) create the infusion builder
 	infusion_builder = SCons.Builder.Builder(
-#		action = env.Action(infusion_action, infusion_string),
 		generator = infusion_action_generator,
 		emitter = infusion_emitter,
 		target_factory = SCons.Node.FS.Dir,
 		source_factory = SCons.Node.FS.Entry)
 
 	env.Append(BUILDERS = { 'Infusion': infusion_builder })
-
-	# 4) InfusionArchiveObject builder
-	env.AddMethod(infusion_archive_object_method, "InfusionArchiveObject")
 
 def exists(env):
 	return 1
