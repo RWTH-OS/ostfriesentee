cb387d11874987019fd060722a1c29765b16897e
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-05-18 18:43:22 +0200

    [architecture] add architecture specific time type

---
+++ architecture/amd64/time.h
@@ -0,0 +1,23 @@
+/*
+ * time.h
+ *
+ * Copyright (c) 2015 Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
+ *
+ * This file is part of Ostfriesentee.
+ *
+ * Ostfriesentee is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ostfriesentee is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Ostfriesentee.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// use 64-bit values to store time
+typedef int64_t dj_time_t;
