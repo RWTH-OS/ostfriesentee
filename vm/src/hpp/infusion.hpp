957c2df7279e368e60daed03c34fc2d2749ad935
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-12 13:02:37 +0200

    [vm] infusion.hpp: add class to inspect lists

--- vm/src/hpp/infusion.hpp
+++ vm/src/hpp/infusion.hpp
@@ -3,6 +3,33 @@
 
 namespace ostfriesentee {
 
+
+class List {
+	dj_di_pointer list;
+
+	/// returns a ref_t that contains the offset of the element
+	/// refered to by the index
+	const ref_t getOffset(const uint8_t index) const {
+		// see dj_di_parentElement_getChild for why the `+1` is needed
+		return dj_di_getListPointer(this->list + 1, index);
+	}
+
+public:
+	List(const dj_di_pointer list) {
+		this->list = list;
+	}
+
+	uint8_t getSize() const {
+		return dj_di_parentElement_getListSize(this->list);
+	}
+
+	// index must be smaller than value returned by getSize()
+	const dj_di_pointer getElement(const uint8_t index) const {
+		return (this->list + this->getOffset(index));
+	}
+
+};
+
 class Infusion {
 	dj_infusion* inf;
 
@@ -39,6 +66,17 @@ public:
 		return reinterpret_cast<char*>(dj_di_header_getInfusionName(this->inf->header));
 	}
 
+	//------------------------------------------------------------------------
+
+	const List getClassList() {
+		List classList(this->inf->classList);
+		return classList;
+	}
+
+	const List getMethodImplementationList() {
+		List methodImplementationList(this->inf->methodImplementationList);
+		return methodImplementationList;
+	}
 };
 
 } // namespace ostfriesentee
