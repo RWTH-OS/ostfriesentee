1bd19cd50974180e8139768d84c29bdff52d0f83
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-21 21:48:51 +0200

    [vm] object.hpp: add methods to get a thread to execute and always use runtime vm

--- vm/src/hpp/object.hpp
+++ vm/src/hpp/object.hpp
@@ -8,15 +8,24 @@ namespace ostfriesentee {
 class Object {
 protected:
 	/// creates a dj_object instance
-	static inline dj_object* create(Vm& vm, dj_infusion* infusion, uint8_t classId) {
+	static inline dj_object* create(dj_infusion* infusion, uint8_t classId) {
 		dj_global_id id {infusion, classId};
 		uint8_t runtime_id = dj_global_id_getRuntimeClassId(id);
-		dj_di_pointer classDef = vm.getRuntimeClassDefinition(runtime_id);
+		dj_di_pointer classDef =
+			dj_vm_getRuntimeClassDefinition(dj_exec_getVM(), runtime_id);
 		dj_object* obj = dj_object_create(runtime_id,
 				dj_di_classDefinition_getNrRefs(classDef),
 				dj_di_classDefinition_getOffsetOfFirstReference(classDef)
 				);
 		return obj;
+	}
+
+	static inline dj_thread* getThread() {
+		dj_vm* vm = dj_exec_getVM();
+		if(vm->threads == nullptr) {
+			vm->threads = dj_thread_create();
+		}
+		return vm->threads;
 	}
 };
 
