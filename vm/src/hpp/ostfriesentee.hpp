0b47978d81e4fdfe854fba6d17d5725836b4b8e0
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-24 21:47:15 +0200

    [vm] hpp: include c headerfiles in cpp wrapper

--- vm/src/hpp/ostfriesentee.hpp
+++ vm/src/hpp/ostfriesentee.hpp
@@ -1,6 +1,12 @@
 #ifndef OSTFRIESENTEE_HPP
 #define OSTFRIESENTEE_HPP
 
+#include <types.h>
+#include <vm.h>
+#include <heap.h>
+#include <execution.h>
+#include <config.h>
+#include <pointerwidth.h>
 #include <hpp/vm.hpp>
 #include <hpp/infusion.hpp>
 #include <hpp/object.hpp>
