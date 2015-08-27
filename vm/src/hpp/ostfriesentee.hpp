334c63ca0b1691b6f866ffee51e675431e06b098
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-27 22:07:56 +0200

    [vm] hpp: include panic.h and put extern "C" {} around includes

--- vm/src/hpp/ostfriesentee.hpp
+++ vm/src/hpp/ostfriesentee.hpp
@@ -1,12 +1,15 @@
 #ifndef OSTFRIESENTEE_HPP
 #define OSTFRIESENTEE_HPP
 
+extern "C" {
 #include <types.h>
 #include <vm.h>
 #include <heap.h>
 #include <execution.h>
 #include <config.h>
 #include <pointerwidth.h>
+#include <panic.h>
+}
 #include <hpp/vm.hpp>
 #include <hpp/infusion.hpp>
 #include <hpp/object.hpp>
