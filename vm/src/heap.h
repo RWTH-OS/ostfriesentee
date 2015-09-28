c5fdbaf14cd74adeb914bfd74ffdd9ca86cfdf8d
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-09-28 12:14:41 +0200

    [vm] increase size of safe pointer pool from 4 to 10 entries

--- vm/src/heap.h
+++ vm/src/heap.h
@@ -25,7 +25,7 @@
 #include "types.h"
 
 
-#define SAFE_POINTER_POOL_SIZE 4
+#define SAFE_POINTER_POOL_SIZE 10
 
 /**
  * Heap chunk types. Darjeeling keeps a heap where Java objects and non-Java objects are mixed on the same heap.
